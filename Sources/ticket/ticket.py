048be85f9528d55db6a89005db846d25240ef735
talaboulma walid <talaboulma.walid@gmail.com>
2019-12-03 12:35:07 +0100

    Adds basic structure for webapp (functional webpage) adds a sample toy database

--- Sources/ticket/ticket.py
+++ Sources/ticket/ticket.py
@@ -10,4 +10,41 @@
 # File : File
 # Date Created : Date
 #
+# CREATE TABLE Ticket (
+#    id INT PRIMARY KEY,
+#    change TEXT NOT NULL,
+#    title TEXT NOT NULL,
+#    description TEXT NULL,
+#    submitter_name TEXT NOT NULL,
+#    submitter_email TEXT NOT NULL,
+#    submitter_website TEXT NULL,
+#    file TEXT NULL,
+#    date_created DATE NOT NULL,
+#    );
 
+import sqlite3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def list_tickets():
+    """Display a list of tickets in the system."""
+    return 'list_tickets'
+
+
+@app.route('/ticket', methods=['GET', 'POST'])
+def add_ticket():
+    """Add a new ticket via a form."""
+    return 'add_ticket'
+
+
+@app.route('/ticket/id/<int:ticket_id>')
+def view_ticket(ticket_id):
+    """Display the details of the ticket with id *ticket_id*."""
+    return 'view_ticket : id = {}'.format(ticket_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
