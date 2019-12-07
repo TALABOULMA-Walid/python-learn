d0fbe3eb5d3105816858270cc8116ad92fccdfc6
talaboulma walid <talaboulma.walid@gmail.com>
2019-12-07 17:52:18 +0100

    Small spacing fix

--- Sources/improve/improvegenerators.py
+++ Sources/improve/improvegenerators.py
@@ -1,8 +1,10 @@
 import random
 
+
 def get_data():
     """Return 3 random integers between 0 and 9"""
     return random.sample(range(10), 3)
+
 
 def consume():
     """Displays a running average across lists of integers sent to it"""
@@ -12,8 +14,10 @@ def consume():
     while True:
         data = yield
         data_items_seen += len(data)
+        data = yield
         running_sum += sum(data)
         print('The running average is {}'.format(running_sum / float(data_items_seen)))
+
 
 def produce(consumer):
     """Produces a set of values and forwards them to the pre-defined consumer
@@ -24,11 +28,11 @@ def produce(consumer):
         consumer.send(data)
         yield
 
+
 if __name__ == '__main__':
     consumer = consume()
     consumer.send(None)
     producer = produce(consumer)
-
     for _ in range(10):
         print('Producing...')
         next(producer)
