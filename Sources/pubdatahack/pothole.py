e77c6129607f9961d94d1518a8782561722da626
talaboulma walid <talaboulma.walid@gmail.com>
2019-12-10 18:49:27 +0100

    Adds 10 worst potholes and lambda function

--- Sources/pubdatahack/pothole.py
+++ Sources/pubdatahack/pothole.py
@@ -9,7 +9,7 @@ with open("potholes.csv") as f:
         street_address = re.split('^[0-9]* ', row['STREET ADDRESS'])[-1]
         Streets.setdefault(street_address, 0)
         try:
-            Streets[street_address] += float(potholes)
+            Streets[street_address] += 1  # float(potholes)
             if Streets[street_address] > max_potholes:
                 max_potholes = Streets[street_address]
                 worst_street = street_address
@@ -17,3 +17,14 @@ with open("potholes.csv") as f:
             pass
 
 print(worst_street, max_potholes)
+
+# worst 10 streets
+street_list = []
+for key, value in Streets.items():
+    street_list.append((key, value))
+street_list.sort(key=lambda x: x[1])  # or sorted(street_list, ...)
+for st in street_list[-1:-10:-1]:
+    print(st)
+
+# interesting:
+# sorted(street_list[800:810], key=lambda x:(random.randint(1,10),x[1]), reverse=True)
