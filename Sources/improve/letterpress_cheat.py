effb1175554d170df58af3b26e3df1959d3c1e8f
talaboulma walid <talaboulma.walid@gmail.com>
2020-01-05 19:18:36 +0100

    changes dictionary key type to tuple , to avoid doing join on list it's faster. Also use pickle to store the result in .pickle file somehow handles exception

--- Sources/improve/letterpress_cheat.py
+++ Sources/improve/letterpress_cheat.py
@@ -1,34 +1,40 @@
 # use python -m cProfile letterpress_cheat.py
 import pickle
-# use pickle.dump to save to a file, and pickle.load(file) to load afterward
+from itertools import combinations
 
-words_anagram_dict = {}
-words_anagrams = []
+PICKLE_WORD_FILE = "anagram-dictionary.pickle"
+WORDS_FILE = "/usr/share/dict/words"
 
 
 def create_anagram_dict():
-    with open("/usr/share/dict/words", "r") as words_file:
-        for word in words_file.readlines():
-            word = word.lower().rstrip()
-            if word.isalpha():
-                word_anagram = ''.join(sorted(word))
-                words_anagram_dict.setdefault(word_anagram, []).append(word)
-# Computationally Heavy
-#               if word_anagram not in words_anagrams:
-#                   words_anagrams.append(word_anagram)
-#       words_anagrams.sort()
+    """Creates a dictionary of all possible anagrams associated with their corresponding words"""
+    try:
+        with open(WORDS_FILE, "r") as words_file:
+            words_anagram_dict = {}
+            for word in words_file.readlines():
+                word = word.lower().rstrip()
+                if word.isalpha() and (2 < len(word) < 12):
+                    word_anagram = tuple(sorted(word))
+                    words_anagram_dict.setdefault(word_anagram, []).append(word)
+        return words_anagram_dict
+    except FileNotFoundError:
+        print("words file not found, maybe you are not in a Linux system,"
+              "\ntry modify code to use another path for file\n")
 
 
-#def save_anagram_dict():
-#    with open("./anagrams.txt", "w") as anagram_file:
-#        for anagram in words_anagram_dict:
-#            anagram_file.write('{anagram} : {words} \n'.format(
-#                anagram=anagram,
-#                words=' '.join(words_anagram_dict[anagram]))
-#            )
+def find_possible_words(anagrams, available_letters):
+    """return an ordered list of all possible words that can be made with available letters"""
+    found_words = []
+
+    return sorted(found_words, key=lambda x: len(x))
 
 
 if __name__ == '__main__':
-    create_anagram_dict()
+    try:
+        with open(PICKLE_WORD_FILE, "rb") as f:
+            words_anagrams = pickle.load(f)
+    except FileNotFoundError:
+        words_anagrams = create_anagram_dict()
+        pickle.dump(words_anagrams, open(PICKLE_WORD_FILE, "wb"))
 
 
